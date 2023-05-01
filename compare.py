8a2866e4cbc84fbba8c4d9898d6e0569172b002a
Ansible Network Automation <127642662+ansnetauto@users.noreply.github.com>
2023-05-01 21:47:34 +1000

    Add files via upload

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@ file01 = "httpd.conf .bak"
 file02 = "httpd.conf"
 
 # Read the contents of the first file
-with open(f'./{file01}', 'r') as f1: 
+with open(f'./{file01}', 'r') as f1:
     file1_contents = f1.readlines()
 
 # Read the contents of the second file
@@ -16,5 +16,5 @@ with open(f'./{file02}', 'r') as f2:
 differences = difflib.HtmlDiff(wrapcolumn=79).make_file(file1_contents, file2_contents)
 
 # Write the differences to an HTML file
-with open(f'./{file01}_{file02}_compared.html', 'w') as html_file.:
+with open(f'./{file01}_{file02}_compared.html', 'w') as html_file:
     html_file.write(differences)
